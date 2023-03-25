7bda39483eb043c142377b917485ea7fc2634cae
Asma <asmaboubaker11@gmail.com>
2023-03-25 22:58:33 +0100

    add found pet

--- src/Components/Pet/AddLostOrFound.js
+++ src/Components/Pet/AddLostOrFound.js
@@ -1,29 +1,37 @@
 
-import React, {useEffect} from "react";
-import {useState} from "react";
-import {useDispatch} from "react-redux";
-import {createBrowserHistory} from 'history';
-import {addPetc} from "./SlicePet";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { createBrowserHistory } from 'history';
+import { addPetc } from "./SlicePet";
 import axios from "axios";
 function Addlostorfound() {
     const history = createBrowserHistory();
     const dispatch = useDispatch();
 
     const [color, setColor] = useState("");
-    const [name, setName] = useState("");
+    const [type, setType] = useState("");
     const [location, setLocation] = useState("");
     const [image, setImage] = useState([]);
-    const [description, setDescription] = useState([]);
+    const [description, setDescription] = useState("");
     const userFromLocalStorageString = localStorage.getItem('user');
     const user = userFromLocalStorageString ? JSON.parse(userFromLocalStorageString) : null;
-   
+
     const handleImageChange = (event) => {
-     
-       setImage([
+
+        setImage([
             ...image,
             ...event.target.files
         ]);
-       
+
+    };
+    const handleTypeChange = (e) => {
+        console.log(e.target.value);
+        setType(e.target.value);
+    };
+    const handlelocationChange = (e) => {
+        console.log(e.target.value);
+        setLocation(e.target.value);
     };
     const handleSubmit = async (event) => {
 
@@ -31,24 +39,26 @@ function Addlostorfound() {
         event.preventDefault();
 
         const formData = new FormData();
-        // formData.append("description", description);
+        formData.append("description", description);
+        formData.append("location", location);
         formData.append("color", color);
+        formData.append("type", type);
         formData.append("user", JSON.stringify(user));
-       for (let i = 0; i < image.length; i++) {
+        for (let i = 0; i < image.length; i++) {
             formData.append("image", image[i]);
         }
-       
+
         const url = 'http://127.0.0.1:3000/pet/addlost'
         axios.post(url, formData).then(data => {
-           
-           
+
+
             history.push('/listlost');
             window.location.reload();
         }).catch(error => {
             console.error(error);
         });
     };
-    
+
     return (
         <div>
 
@@ -90,81 +100,96 @@ function Addlostorfound() {
                                 <h4 className="title-25 mb-20">Billing Details</h4>
                                 <form onSubmit={handleSubmit}>
                                     <div className="row">
-                                    <div className="col-12">
+                                        <div className="col-12">
                                             <div className="form-inner">
-                                            <label>Type of operation</label>
-                                                <div className="nice-select" ><span className="current">Town / City</span>
-                                                    <ul className="list">
-                                                        <li className="option selected">Lost / Found</li>
-                                                        <li className="option">Lost</li>
-                                                        <li className="option">Found</li>
-                                                      
-                                                    </ul>
-                                                </div>
+                                                <label>Type of operation</label>
+                                                <select id="type" name="type" value={type} onChange={handleTypeChange}>
+                                                    <option value="">Select a type</option>
+                                                    <option value="lost">Lost</option>
+                                                    <option value="found">Found</option>
+                                                </select>
                                             </div>
                                         </div>
-                                        
+
                                         <div className="col-12">
                                             <div className="form-inner">
                                                 <label>Images</label>
 
                                                 <input type="file" name="image" id="file-input"
-                                            onChange={handleImageChange}
-                                            />
-                                        
-                                               
+                                                    onChange={handleImageChange}
+                                                />
+
+
                                             </div>
                                         </div>
                                         <div className="col-12">
                                             <div className="form-inner">
                                                 <label>Color </label>
-                                                <input type="text" name="color" placeholder="color pet " 
-                                                
-                                                value={color}
-                                                onChange={
-                                                    (event) => {
-                                                        setColor(event.target.value);
+                                                <input type="text" name="color" placeholder="color pet "
+
+                                                    value={color}
+                                                    onChange={
+                                                        (event) => {
+                                                            setColor(event.target.value);
+                                                        }
                                                     }
-                                                }
                                                 />
                                             </div>
                                         </div>
                                         <div className="col-12">
                                             <div className="form-inner">
                                                 <label>Street Address</label>
-                                                <div className="nice-select" ><span className="current">Town / City</span>
-                                                    <ul className="list">
-                                                        <li className="option selected">Town / City</li>
-                                                        <li className="option">Dhaka</li>
-                                                        <li className="option">Saidpur</li>
-                                                        <li className="option">Newyork</li>
-                                                    </ul>
-                                                </div>
+                                                <select  name="location" value={location} onChange={handlelocationChange}>
+                                                    <option value="">Town / City</option>
+                                                    <option value="Tunis">Tunis</option>
+                                                    <option value="Sfax">Sfax</option>
+                                                    <option value="Sousse">Sousse</option>
+                                                    <option value="Gabes">Gabes</option>
+                                                    <option value="Bizerte">Bizerte</option>
+                                                    <option value="Ariana">Ariana</option>
+                                                    <option value="Ben Arous">Ben Arous</option>
+                                                    <option value="Nabeul">Nabeul</option>
+                                                    <option value="Monastir">Monastir</option>
+                                                    <option value="Mahdia">Mahdia</option>
+                                                    <option value="Kairouan">Kairouan</option>
+                                                    <option value="Tozeur">Tozeur</option>
+                                                    <option value="Kasserine">Kasserine</option>
+                                                    <option value="Gafsa">Gafsa</option>
+                                                    <option value="Tataouine">Tataouine</option>
+                                                    <option value="Zaghouan">Zaghouan</option>
+                                                    <option value="Kebili">Kebili</option>
+                                                    <option value="Jendouba">Jendouba</option>
+                                                    <option value="Siliana">Siliana</option>
+                                                    <option value="Beja">Beja</option>
+                                                    <option value="Kef">Kef</option>
+                                                    <option value="Sidi Bouzid">Sidi Bouzid</option>
+                                                    <option value="Medenine">Medenine</option>
+                                                    <option value="Gassrine">Gassrine</option>
+                                                    <option value="Manouba">Manouba</option>
+                                                </select>
+
                                             </div>
                                         </div>
-                                       
-                                      
+
+
                                         <div className="col-12">
                                             <div className="form-inner">
                                                 <label>Additional Information</label>
-                                                <input type="text" name="fname" placeholder="Your Phone Number" />
-                                            </div>
+                                                <div className="form-inner">
+                                                    <textarea name="description" placeholder="Order Notes (Optional)" rows="6"
+
+                                                        value={description}
+                                                        onChange={
+                                                            (event) => {
+
+                                                                setDescription(event.target.value);
+                                                            }
+                                                        }
+                                                    ></textarea>
+                                                </div>                                            </div>
                                         </div>
-                                       
-                                       
-                                        <div className="col-12">
-                                            <div className="form-inner">
-                                                {/* <textarea name="description" placeholder="Order Notes (Optional)" rows="6"
-                                                
-                                                value={description}
-                                                onChange={
-                                                    (event) => {
-                                                        setDescription(event.target.description);
-                                                    }
-                                                }
-                                                ></textarea> */}
-                                            </div>
-                                        </div>
+
+
                                     </div>
                                     <div className="place-order-btn">
                                         <button type="submit" className="primary-btn1 lg-btn">Place Order</button>
@@ -248,9 +273,9 @@ function Addlostorfound() {
                                     </li>
                                 </ul>
                             </div>
-                            
-                            
-                            
+
+
+
                         </aside>
                     </div>
                 </div>
